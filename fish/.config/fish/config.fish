8594529233f39569e75340431e2dabaf1e6b19aa
Billy Rieger <wrieger@protonmail.com>
2020-08-06 10:42:28 -0400

    New fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,13 @@
-set -x PATH $PATH ~/.cargo/bin
+set -x PATH ~/.cargo/bin ~/.bin $PATH
 set -x FZF_DEFAULT_COMMAND 'fd --type f'
+set -x MANPAGER 'nvim +Man!'
+set -x MANWIDTH 999
+
 source ~/.asdf/asdf.fish
 
-alias lsd 'lsd --group-dirs first'
+#    --classify           # append helpful indicator to entries
+#    --group-dirs first   # sort directories before files
+#    --icon never         # don't require fancy patched fonts
+alias lsd 'lsd --classify --group-dirs first --icon never'
 
 starship init fish | source
